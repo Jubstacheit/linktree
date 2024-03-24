bff390052ee7517edcf327ab2970252a2ba37d26
Viviane <dessaintviviane@gmail.com>
2024-03-24 16:52:23 +0100

    Update layout and link card styles

--- app/layout.js
+++ app/layout.js
@@ -11,7 +11,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={`${inter.className} bg-gradient-to-r from-pink-300 via-purple-300 to-indigo-400 text-white`}>{children}</body>
     </html>
   );
 }
--- app/page.js
+++ app/page.js
@@ -7,9 +7,18 @@ function LinkCard({
   image 
 }) {
   return (
-    <a href={href} className="flex items-center p-4 rounded-lg shadow-lg hover:shadow-xl transition-shadow duration-300">
-      <div className="flex flex-col">
-        <h2 className="font-bold text-lg">
+    <a href={href} className="flex items-center w-full p-1 rounded-md shadow-lg hover:scale-105 transition-all bg-gray-100 mb-3">
+      <div className="flex text-center w-full">
+        { image && (
+          <Image
+          alt={title}
+          src={image}
+          width={40}
+          height={40}
+          className="rounded-sm"
+        />
+        )}
+        <h2 className="font-semibold w-full text-center text-gray-700">
           {title}
         </h2>
       </div>
@@ -19,7 +28,7 @@ function LinkCard({
 
 export default function Home() {
   return (
-    <div className="flex items-center flex-col mx-auto w-full justify-center mt-16">
+    <div className="flex items-center flex-col mx-auto w-full justify-center mt-16 px-8">
       <Image
         alt={data.name}
         src={data.avatar}
@@ -27,7 +36,7 @@ export default function Home() {
         height={96}
         className="rounded-full"
       />
-      <h1 className="font-bold mt-4 text-xl">
+      <h1 className="font-bold mt-4 text-xl mb-8">
         {data.name}
       </h1>
       {data.links.map((link) => (
