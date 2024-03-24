8fd2b31d9cfa36f33b0ccc801c8f5e4e150c3a84
Viviane <dessaintviviane@gmail.com>
2024-03-24 16:01:17 +0100

    Update styling and layout in page.js

--- app/page.js
+++ app/page.js
@@ -3,15 +3,15 @@ import data from "../data.json";
 
 export default function Home() {
   return (
-    <div className="flex mx-auto w-full justify-center mt-16">
+    <div className="flex items-center flex-col mx-auto w-full justify-center mt-16">
       <Image
         alt={data.name}
         src={data.avatar}
-        width={80}
-        height={80}
+        width={96}
+        height={96}
         className="rounded-full"
       />
-      <h1 className="font-semibold">
+      <h1 className="font-bold mt-4 text-xl">
         {data.name}
       </h1>
     </div>
