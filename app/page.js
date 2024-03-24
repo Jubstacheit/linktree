8780660cc9b760abddc3ce0ef2d2e82bedb33e9b
Viviane <dessaintviviane@gmail.com>
2024-03-24 02:27:19 +0100

    Add avatar and name to Home page

--- app/page.js
+++ app/page.js
@@ -1,9 +1,19 @@
 import Image from "next/image";
+import data from "../data.json";
 
 export default function Home() {
   return (
-    <div>
-      <h1>Home</h1>
+    <div className="flex mx-auto w-full justify-center mt-16">
+      <Image
+        alt={data.name}
+        src={data.avatar}
+        width={80}
+        height={80}
+        className="rounded-full"
+      />
+      <h1 className="font-semibold">
+        {data.name}
+      </h1>
     </div>
   );
 }
