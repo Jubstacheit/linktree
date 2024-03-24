be50ee0585b4a774f62df36293d1090415f8337c
Viviane <dessaintviviane@gmail.com>
2024-03-24 18:00:19 +0100

    Update title in metadata

--- app/layout.js
+++ app/layout.js
@@ -4,7 +4,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Jubstacheit",
+  title: "Jubstacheit - My Links",
   description: "Links and socials for Jubstacheit",
 };
 
