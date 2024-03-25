770b29190e1650fbb18b1e32af7406713deaedcb
Viviane <dessaintviviane@gmail.com>
2024-03-25 16:39:09 +0100

    Add social media icons and link cards

--- app/page.js
+++ app/page.js
@@ -5,19 +5,20 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 export const dynamic = 'force-dynamic'
 
+// Different icons for social media
 function TwitterIcon() {
   return (
     <svg
       width="30"
       height="24"
       viewBox="0 0 24 20"
-      fill="none"
+      fill='#1DA1F2'
       xmlns="http://www.w3.org/2000/svg"
     >
       <g clipPath="url(#a)">
         <path
           d="M21.479 4.937c.015.209.015.418.015.628 0 6.424-4.917 13.832-13.906 13.832v-.004a13.89 13.89 0 0 1-7.491-2.18 9.847 9.847 0 0 0 7.233-2.015 4.89 4.89 0 0 1-4.566-3.375c.732.14 1.487.112 2.206-.084a4.868 4.868 0 0 1-3.92-4.764v-.062c.68.376 1.44.585 2.218.608a4.851 4.851 0 0 1-1.513-6.49 13.896 13.896 0 0 0 10.073 5.078 4.848 4.848 0 0 1 1.414-4.644 4.911 4.911 0 0 1 6.914.21A9.84 9.84 0 0 0 23.26.496a4.884 4.884 0 0 1-2.149 2.69 9.76 9.76 0 0 0 2.807-.766 9.898 9.898 0 0 1-2.439 2.518Z"
-          fill="currentColor"
+          fill="#1DA1F2"
         />
       </g>
       <defs>
@@ -35,13 +36,13 @@ function GitHubIcon() {
       width="30"
       height="30"
       viewBox="0 0 24 24"
-      fill="none"
+      fill="black"
       xmlns="http://www.w3.org/2000/svg"
     >
       <g clipPath="url(#clip0_9914_10)">
         <path
           d="M12 0C5.374 0 0 5.373 0 12C0 17.302 3.438 21.8 8.207 23.387C8.806 23.498 9 23.126 9 22.81V20.576C5.662 21.302 4.967 19.16 4.967 19.16C4.421 17.773 3.634 17.404 3.634 17.404C2.545 16.659 3.717 16.675 3.717 16.675C4.922 16.759 5.556 17.912 5.556 17.912C6.626 19.746 8.363 19.216 9.048 18.909C9.155 18.134 9.466 17.604 9.81 17.305C7.145 17 4.343 15.971 4.343 11.374C4.343 10.063 4.812 8.993 5.579 8.153C5.455 7.85 5.044 6.629 5.696 4.977C5.696 4.977 6.704 4.655 8.997 6.207C9.954 5.941 10.98 5.808 12 5.803C13.02 5.808 14.047 5.941 15.006 6.207C17.297 4.655 18.303 4.977 18.303 4.977C18.956 6.63 18.545 7.851 18.421 8.153C19.191 8.993 19.656 10.064 19.656 11.374C19.656 15.983 16.849 16.998 14.177 17.295C14.607 17.667 15 18.397 15 19.517V22.81C15 23.129 15.192 23.504 15.801 23.386C20.566 21.797 24 17.3 24 12C24 5.373 18.627 0 12 0Z"
-          fill="currentColor"
+          fill="black"
         />
       </g>
       <defs>
@@ -60,7 +61,7 @@ function MastoIcon() {
       viewBox="0 0 448 512"
       width="30"
       height="30"
-      fill="white"
+      fill="#8C8DFF"
     >
       <path d="M433 179.1c0-97.2-63.7-125.7-63.7-125.7-62.5-28.7-228.6-28.4-290.5 0 0 0-63.7 28.5-63.7 125.7 0 115.7-6.6 259.4 105.6 289.1 40.5 10.7 75.3 13 103.3 11.4 50.8-2.8 79.3-18.1 79.3-18.1l-1.7-36.9s-36.3 11.4-77.1 10.1c-40.4-1.4-83-4.4-89.6-54a102.5 102.5 0 0 1 -.9-13.9c85.6 20.9 158.7 9.1 178.8 6.7 56.1-6.7 105-41.3 111.2-72.9 9.8-49.8 9-121.5 9-121.5zm-75.1 125.2h-46.6v-114.2c0-49.7-64-51.6-64 6.9v62.5h-46.3V197c0-58.5-64-56.6-64-6.9v114.2H90.2c0-122.1-5.2-147.9 18.4-175 25.9-28.9 79.8-30.8 103.8 6.1l11.6 19.5 11.6-19.5c24.1-37.1 78.1-34.8 103.8-6.1 23.7 27.3 18.4 53 18.4 175z"/></svg>
   )
@@ -73,7 +74,7 @@ function InstaIcon() {
       viewBox="0 0 448 512"
       width="30"
       height="30"
-      fill="white"
+      fill="#E1306C"
     >
     <path d="M224.1 141c-63.6 0-114.9 51.3-114.9 114.9s51.3 114.9 114.9 114.9S339 319.5 339 255.9 287.7 141 224.1 141zm0 189.6c-41.1 0-74.7-33.5-74.7-74.7s33.5-74.7 74.7-74.7 74.7 33.5 74.7 74.7-33.6 74.7-74.7 74.7zm146.4-194.3c0 14.9-12 26.8-26.8 26.8-14.9 0-26.8-12-26.8-26.8s12-26.8 26.8-26.8 26.8 12 26.8 26.8zm76.1 27.2c-1.7-35.9-9.9-67.7-36.2-93.9-26.2-26.2-58-34.4-93.9-36.2-37-2.1-147.9-2.1-184.9 0-35.8 1.7-67.6 9.9-93.9 36.1s-34.4 58-36.2 93.9c-2.1 37-2.1 147.9 0 184.9 1.7 35.9 9.9 67.7 36.2 93.9s58 34.4 93.9 36.2c37 2.1 147.9 2.1 184.9 0 35.9-1.7 67.7-9.9 93.9-36.2 26.2-26.2 34.4-58 36.2-93.9 2.1-37 2.1-147.8 0-184.8zM398.8 388c-7.8 19.6-22.9 34.7-42.6 42.6-29.5 11.7-99.5 9-132.1 9s-102.7 2.6-132.1-9c-19.6-7.8-34.7-22.9-42.6-42.6-11.7-29.5-9-99.5-9-132.1s-2.6-102.7 9-132.1c7.8-19.6 22.9-34.7 42.6-42.6 29.5-11.7 99.5-9 132.1-9s102.7-2.6 132.1 9c19.6 7.8 34.7 22.9 42.6 42.6 11.7 29.5 9 99.5 9 132.1s2.7 102.7-9 132.1z"/></svg>
   )
@@ -86,18 +87,19 @@ function BskyIcon() {
       viewBox="0 0 512 512"
       width="30"
       height="30"
-      fill="white"
+      fill="#00B0FF"
     ><path d="M160.8 96.5c14 17 31 30.9 49.5 42.2c25.9 15.8 53.7 25.9 77.7 31.6V138.8C265.8 108.5 250 71.5 248.6 28c-.4-11.3-7.5-21.5-18.4-24.4c-7.6-2-15.8-.2-21 5.8c-13.3 15.4-32.7 44.6-48.4 87.2zM320 144v30.6l0 0v1.3l0 0 0 32.1c-60.8-5.1-185-43.8-219.3-157.2C97.4 40 87.9 32 76.6 32c-7.9 0-15.3 3.9-18.8 11C46.8 65.9 32 112.1 32 176c0 116.9 80.1 180.5 118.4 202.8L11.8 416.6C6.7 418 2.6 421.8 .9 426.8s-.8 10.6 2.3 14.8C21.7 466.2 77.3 512 160 512c3.6 0 7.2-1.2 10-3.5L245.6 448H320c88.4 0 160-71.6 160-160V128l29.9-44.9c1.3-2 2.1-4.4 2.1-6.8c0-6.8-5.5-12.3-12.3-12.3H400c-44.2 0-80 35.8-80 80zm80-16a16 16 0 1 1 0 32 16 16 0 1 1 0-32z"/></svg>
   )
 }
 
+// Link cards
 function LinkCard({ 
   title, 
   href, 
   image 
 }) {
   return (
-    <a href={href} className="flex items-center w-full p-1 rounded-md shadow-lg hover:scale-105 transition-all max-w-3xl bg-gray-100 mb-3">
+    <a href={href} className="flex items-center w-full p-1 rounded-md shadow-lg hover:scale-105 hover:bg-gray-200 hover:rounded-none transition-all max-w-3xl bg-gray-100 mb-3">
       <div className="flex text-center w-full">
         <div className="w-10 h-10">
           { image && (
@@ -118,9 +120,12 @@ function LinkCard({
   )
 }
 
+
+// Main app
 export default async function Home() {
   const data = await get("linktree");
 
+  // Security in case of missing data from Vercel
   if (!data) {
     redirect("https://twitter.com/JubstacheitAlt");
   }
@@ -133,53 +138,59 @@ export default async function Home() {
         height={96}
         className="rounded-full"
       />
-      <h1 className="font-bold mt-4 text-xl mb-8">
+      <h1 className="font-bold mt-4 text-xl">
         {data.name}
       </h1>
+      <h2 className="font-semibold mt-2 text-l mb-8">
+        {data.subtitle}
+      </h2>
 
-      {data.links.map((link) => (
-        <LinkCard key={link.href} {...link} />
-      ))}
 
-      <div className="flex gap-4 items-center mt-8">
+      <div className="flex gap-4 items-center mb-8">
         {data.socials.map((link) => {
           if (link.href.includes("twitter")) {
             return (
-              <a href={link.href} key={link}>
+              <a className="hover:scale-105 transition-all" href={link.href} key={link}>
                 <TwitterIcon />
               </a>
             )
           }
           if (link.href.includes("github")) {
             return (
-              <a href={link.href} key={link}>
+              <a className="hover:scale-105 transition-all" href={link.href} key={link}>
                 <GitHubIcon />
               </a>
             )
           }
           if (link.href.includes("rivals")) {
             return (
-              <a href={link.href} key={link}>
+              <a className="hover:scale-105 transition-all" href={link.href} key={link}>
                 <MastoIcon />
               </a>
             )
           }
           if (link.href.includes("instagram")) {
             return (
-              <a href={link.href} key={link}>
+              <a className="hover:scale-105 transition-all" href={link.href} key={link}>
                 <InstaIcon />
               </a>
             )
           }
           if (link.href.includes("bsky")) {
             return (
-              <a href={link.href} key={link}>
+              <a className="hover:scale-105 transition-all" href={link.href} key={link}>
                 <BskyIcon />
               </a>
             )
           }
         })}
       </div>
+
+      {data.links.map((link) => (
+        <LinkCard key={link.href} {...link} />
+      ))}
+
+      
     </div>
   );
 }
